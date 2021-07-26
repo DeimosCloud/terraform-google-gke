b66ccd726742ce71deeb2fff74d226d812ec9dc4
Mmadu Manasseh <mmadu@deimos.co.za>
2021-07-27 00:54:14 +0100

    [Node pool] create before destroy

--- modules/gke-node-pool/main.tf
+++ modules/gke-node-pool/main.tf
@@ -78,7 +78,8 @@ resource "google_container_node_pool" "node_pool" {
 
 
   lifecycle {
-    ignore_changes = [initial_node_count]
+    ignore_changes        = [initial_node_count]
+    create_before_destroy = true
   }
 
   timeouts {
